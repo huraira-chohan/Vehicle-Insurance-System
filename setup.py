0231c94bae311d24864e872537147b696abe6f77
Huraira <abuhurairachohan2006@gmail.com>
2025-10-03 16:53:48 +0500

    Proj Structure Ready-venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Abu huraira Chohan',
+    author_email='abuhurairachohan2006@gmail.com',
+    packages=find_packages()
+    )
+
